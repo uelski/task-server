a152d55444d2563e588c7697e2fd6d4d65592977
Sam Vredenburgh <sam.vburgh@gmail.com>
2025-12-18 17:19:54 -0700

    building out routes with prisma integration

--- src/routes/projects.routes.ts
+++ src/routes/projects.routes.ts
@@ -1,13 +1,48 @@
 import {Router} from 'express'
+import { prisma } from '../lib/prisma'
 import { projects } from '../helpers/projects.helper'
+
+
 const router = Router()
 
 router.get('/', (req, res) => {
   res.json({ message: 'Hello World' });
 });
 
-router.get('/projects-with-tasks', (req, res) => {
+router.get('/projects-with-tasks', async (req, res) => {
+  const projects = await prisma.project.findMany({
+    include: {
+      tasks: true,
+    },
+  });
   res.json(projects);
+});
+
+router.post('/', async (req, res) => {
+  const { title, description } = req.body;
+  const project = await prisma.project.create({
+    data: { title, description },
+  });
+  res.json(project);
+});
+
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { title, description } = req.body;
+  const project = await prisma.project.update({
+    where: { id: parseInt(id) },
+    data: { title, description },
+  });
+  res.json(project);
+});
+
+//delete with projectid
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  const project = await prisma.project.delete({
+    where: { id: parseInt(id) },
+  });
+  res.json(project);
 });
 
 export default router
--- src/routes/tasks.routes.ts
+++ src/routes/tasks.routes.ts
@@ -1,9 +1,40 @@
 import {Router} from 'express'
+import { prisma } from '../lib/prisma';
 
 const router = Router()
 
 router.get('/', (req, res) => {
   res.json({ message: 'Hello World' });
+});
+
+// post with projectid
+router.post('/:projectId', async (req, res) => {
+  const { title, description } = req.body;
+  const { projectId } = req.params;
+  const task = await prisma.task.create({
+    data: { title, description, projectId: parseInt(projectId) },
+  });
+  res.json(task);
+});
+
+//patch with taskid
+router.patch('/:taskId', async (req, res) => {
+  const { title, description } = req.body;
+  const { taskId } = req.params;
+  const task = await prisma.task.update({
+    where: { id: parseInt(taskId) },
+    data: { title, description },
+  });
+  res.json(task);
+});
+
+//delete with taskid
+router.delete('/:taskId', async (req, res) => {
+  const { taskId } = req.params;
+  const task = await prisma.task.delete({
+    where: { id: parseInt(taskId) },
+  });
+  res.json(task);
 });
 
 export default router
